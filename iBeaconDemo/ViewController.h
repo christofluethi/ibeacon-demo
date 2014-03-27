f28a6ade0b59aadc8019f289eef943283f8b8d37
Christof Luethi <christof.luethi@gmail.com>
2014-03-27 19:17:32 +0100

    initial commit from local git

---
+++ iBeaconDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeaconDemo
+//
+//  Created by Christof Luethi on 27.03.14.
+//  Copyright (c) 2014 shaped.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
